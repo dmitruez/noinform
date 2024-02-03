c89ed792343db6a346ae81de80b064061e90e81b
dmitruez <dyezerski@icloud.com>
2024-02-03 20:42:47 +0300

    умножение

--- calculator.py
+++ calculator.py
@@ -7,6 +7,10 @@ class Calculator:
 		self.last_res = n1 + n2
 		return n1 + n2
 	
+	def multiply(self, n1, n2):
+		self.last_res = n1 * n2
+		return n1 * n2
+	
 	def printify(self):
 		print(self.last_res)
 		
